d644573c83078e71d5f7dc69bdc56416430b1858
alejandro terrill <you@example.com>
2019-03-10 09:01:34 -0500

    ok

---
+++ sentence-server-solution/src/main/java/demo/service/SentenceService.java
@@ -0,0 +1,6 @@
+package demo.service;
+
+public interface SentenceService {
+
+    String buildSentence(); 
+}
---
+++ word-server/src/main/java/demo/WordController.java
@@ -0,0 +1,35 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.domain.Word;
+
+@Controller
+public class WordController {
+
+	@Value("${words}") String words;
+	@Value("${eureka.instance.instanceId}") String instanceId;
+	
+	@GetMapping("/")
+	public @ResponseBody Word getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+		return new Word(wordArray[i]); // + instanceId;
+	}
+
+	@Value("${spring.application.name}") private String appName;
+	@GetMapping("/name")
+	public @ResponseBody String getName() {
+		return appName;
+	}
+
+	@Value("${spring.profiles}") private String profiles;
+	@GetMapping("/profiles")
+	public @ResponseBody String profiles() {
+		return profiles;
+	}
+	
+}
