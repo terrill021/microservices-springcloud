8b7c69a6d8b8e97513300b00b611d4df624591b9
alejandro.terrill <alejandro.terrill@ceiba.com.co>
2019-05-03 14:58:47 -0500

    agregar windows bat

---
+++ word-server/windows.bat
@@ -0,0 +1,5 @@
+mvn spring-boot:run -Drun.profiles=verb &
+mvn spring-boot:run -Drun.profiles=article &
+mvn spring-boot:run -Drun.profiles=adjective &
+mvn spring-boot:run -Drun.profiles=noun &
+mvn spring-boot:run -Drun.profiles=subject
